6233c2b6e6cf2a2d6b10fd83aaa6beadcf2a897f
ykobayashi-techno <ykobayashi@it-nasu.com>
2019-11-21 11:48:40 +0900

    サンプル文字に置き換え

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,11 @@
-import env from './../env.json';
-
 export const environment = {
   production: true,
   firebase: {
-    apiKey: env.FIREBASE_API_KEY,
-    authDomain: env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: env.FIREBASE_DATABASE_URL,
-    projectId: env.FIREBASE_PROJECT_ID,
-    storageBucket: env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID
+    apiKey: 'your key',
+    authDomain: 'your key',
+    databaseURL: 'your key',
+    projectId: 'your key',
+    storageBucket: 'your key',
+    messagingSenderId: 'your key'
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,15 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-import env from './../env.json';
-
 export const environment = {
   production: false,
   firebase: {
-    apiKey: env.FIREBASE_API_KEY,
-    authDomain: env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: env.FIREBASE_DATABASE_URL,
-    projectId: env.FIREBASE_PROJECT_ID,
-    storageBucket: env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID
+    apiKey: 'your key',
+    authDomain: 'your key',
+    databaseURL: 'your key',
+    projectId: 'your key',
+    storageBucket: 'your key',
+    messagingSenderId: 'your key'
   }
 };
 
